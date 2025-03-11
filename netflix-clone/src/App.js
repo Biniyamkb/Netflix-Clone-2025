e9191931640009da605cd178525ce1e868e40ef0
Biniyamkb <168299482+Biniyamkb@users.noreply.github.com>
2025-03-11 14:46:46 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import "./index.css";
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Bini</h1>
+      <Home />
     </div>
   );
 }
