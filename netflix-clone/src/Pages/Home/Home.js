9bc9dba28064b7c802d3a4a85747a877714a6dce
Biniyamkb <168299482+Biniyamkb@users.noreply.github.com>
2025-03-12 15:58:54 +0300

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import Baneer from "../../Components/Banner/Baneer";
 
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Baneer/>
       <Footer />
     </>
   );
